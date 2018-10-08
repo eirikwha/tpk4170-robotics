f96c8cba5f1705f84ee892b6c93c0c0b4d87e08f
Lars Tingelstad <lars.tingelstad@ntnu.no>
2018-10-08 16:17:31 +0200

    Changed titles on plots

--- tpk4170/trajectory_planning/trajectory_planning.py
+++ tpk4170/trajectory_planning/trajectory_planning.py
@@ -2,14 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_trajectory(trajectory, iscubic=True):
+def plot_trajectory(trajectory, title=''):
     qs, dqs, ddqs, ts = trajectory
     f, axarr = plt.subplots(3, sharex=True)
     axarr[0].plot(ts, qs, color='red')
-    if iscubic:
-        axarr[0].set_title('Cubic Trajectory')
-    else:
-        axarr[0].set_title('Quintic Trajectory')
+    axarr[0].set_title(title)
     axarr[0].set_ylabel('position')
     axarr[1].plot(ts, dqs, color='green')
     axarr[1].set_ylabel('velocity')
